1d0e110bf9826b3fb743294d3f6c98fe745354eb
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-31 14:43:34 +0530

    add graphql config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.start().then(() => {
+  server.applyMiddleware({ app, path: "/api" });
+
+  app.listen(port);
+
+  console.log(`[app]: http://localhost:${port}`);
 });
-
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
