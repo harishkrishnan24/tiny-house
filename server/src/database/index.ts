83b4b666a90e4038cdfbb1083d854d9bd3563df2
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-09-02 13:06:31 +0530

    connect to db

---
+++ server/src/database/index.ts
@@ -0,0 +1,22 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const cluster = "tiny-house-cluster.ntq3ll9";
+const user = "harishkrishnan1993";
+const userPassword = "u46xIBJU4tPZtkos";
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+  const client = await MongoClient.connect(url, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  const db = client.db("main");
+
+  return {
+    listings: db.collection("test_listings"),
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,26 @@
-import express from "express";
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
+import { connectDatabase } from "./database";
 
-const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
 
-server.start().then(() => {
-  server.applyMiddleware({ app, path: "/api" });
+  server.start().then(async () => {
+    server.applyMiddleware({ app, path: "/api" });
+    app.listen(port);
+    console.log(`[app]: http://localhost:${port}`);
 
-  app.listen(port);
+    const listings = await db.listings.find({}).toArray();
+    console.log(listings);
+  });
+};
 
-  console.log(`[app]: http://localhost:${port}`);
-});
+mount(express());
