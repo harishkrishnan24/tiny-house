2a0510948e92d04d09bd6d4bef4e84143fd55ac3
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-09-29 09:49:57 +0530

    using use reducer for state management

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -12,10 +12,31 @@ type MutationTuple<TData, TVariables> = [
   State<TData>
 ];
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
+const reducer =
+  <TData>() =>
+  (state: State<TData>, action: Action<TData>): State<TData> => {
+    switch (action.type) {
+      case "FETCH":
+        return { ...state, loading: true };
+      case "FETCH_SUCCESS":
+        return { data: action.payload, loading: false, error: false };
+      case "FETCH_ERROR":
+        return { ...state, loading: false, error: true };
+      default:
+        throw new Error("");
+    }
+  };
+
 export const useMutation = <TData = any, TVariables = any>(
   query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false,
@@ -23,7 +44,7 @@ export const useMutation = <TData = any, TVariables = any>(
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({ loading: true, data: null, error: false });
+      dispatch({ type: "FETCH" });
 
       const { data, errors } = await server.fetch<TData, TVariables>({
         query,
@@ -34,9 +55,9 @@ export const useMutation = <TData = any, TVariables = any>(
         throw new Error(errors[0].message);
       }
 
-      setState({ data, loading: false, error: false });
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
     } catch (err) {
-      setState({ data: null, loading: false, error: true });
+      dispatch({ type: "FETCH_ERROR" });
       throw console.error(err);
     }
   };
